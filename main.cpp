621e4f8854eee6b470bad7f3952cf8aa825783a4
Hrishikesh Acharyya <acharyya.hrishi@gmail.com>
2026-08-23 00:11:08 +0530

    Was able to generate colormap and write to a ppm file

--- main.cpp
+++ main.cpp
@@ -24,16 +24,28 @@ vector<RGB> framebuffer(no_of_pixels);//Each RGB struct is one pixel
 int main() {
 
   
-    for (int x = 0; x < VIEWPORT_WIDTH; ++x) {
-        for (int y = 0; y < VIEWPORT_HEIGHT; ++y) {
-            framebuffer[y * VIEWPORT_WIDTH + x] = {uint8_t(x % 256), uint8_t(y % 256), 0};
+    for (int y = 0; y < VIEWPORT_HEIGHT; ++y) {
+        for (int x = 0; x < VIEWPORT_WIDTH; ++x) {
+            framebuffer[y * VIEWPORT_WIDTH + x] = {uint8_t(x % 256), uint8_t(y % 256), 0}; //red-green color map generation
 
-            cout<<"Pixel at (" << x << ", " << y << "): "
-                << "R=" << int(framebuffer[y * VIEWPORT_WIDTH + x].r) << ", "
-                << "G=" << int(framebuffer[y * VIEWPORT_WIDTH + x].g) << ", "
-                << "B=" << int(framebuffer[y * VIEWPORT_WIDTH + x].b) << endl;
+            // cout<<"Pixel at (" << x << ", " << y << "): "
+            //     << "R=" << int(framebuffer[y * VIEWPORT_WIDTH + x].r) << ", " //uint_8 is a char type hence typecasting required
+            //     << "G=" << int(framebuffer[y * VIEWPORT_WIDTH + x].g) << ", "
+            //     << "B=" << int(framebuffer[y * VIEWPORT_WIDTH + x].b) << endl;
         }
     }
 
+    FILE* f = std::fopen("output.ppm", "wb");
+    if (f == nullptr) {
+        cout << "Error: Could not open output.ppm for writing." <<endl;
+        return 1;
+    }
+
+    
+
+      fprintf(f, "P6\n%d %d\n255\n", VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
+      fwrite(reinterpret_cast<const char*>(framebuffer.data()), sizeof(RGB), no_of_pixels, f);
+      fclose(f);
+  
     return 0;
 }
